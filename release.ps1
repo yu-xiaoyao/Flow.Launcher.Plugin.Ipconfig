cec47896d7cd25624aadfafe920de21d718de355
YuXiaoyao <zxyfuncy@gmail.com>
2024-01-22 21:24:37 +0800

    release 1.0.0

--- release.ps1
+++ release.ps1
@@ -1,3 +1,3 @@
 dotnet publish Flow.Launcher.Plugin.Ipconfig -c Release -r win-x64 --no-self-contained
 Copy-Item -Path Flow.Launcher.Plugin.Ipconfig/ipconfig-icon.png -Destination Flow.Launcher.Plugin.Ipconfig/bin/Release/win-x64/publish/ipconfig-icon.png
-Compress-Archive -LiteralPath Flow.Launcher.Plugin.Ipconfig/bin/Release/win-x64/publish -DestinationPath Flow.Launcher.Plugin.Ipconfig/bin/Ipconfig.zip -Force
+Compress-Archive -LiteralPath Flow.Launcher.Plugin.Ipconfig/bin/Release/win-x64/publish -DestinationPath Flow.Launcher.Plugin.Ipconfig/bin/Ipconfig-win-x64.zip -Force
